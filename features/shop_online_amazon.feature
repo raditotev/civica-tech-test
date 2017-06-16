bfcaa5c2d1be6df79dcc9ccdc426e46ca368724d
raditotev <raditotev@gmail.com>
2017-06-16 19:26:26 +0100

    First commit

---
+++ features/shop_online_amazon.feature
@@ -0,0 +1,23 @@
+Feature: Shop online at Amazon.co.uk
+	As amazon registered user
+	I want to be able to purchase goods online
+	So I can save money and time
+
+	Scenario: Sign into Amazon.co.uk
+	Given Amazon.co.uk is open
+	When I click Sign-in
+	And enter valid user name password
+	Then I am logged in
+
+
+	Scenario: Search for product and verify the first result is related to your product
+	Given Amazon.co.uk is open and I am logged in
+	When I search for laptop
+	Then the first result has the word laptop in it
+
+
+	Scenario: Verify that the original price of the product matches the price seen in the basket
+	Given Amazon.co.uk is open and I am logged in
+	And I add laptop ASUS ZenBook UX330UA-FB100T to my basket
+	When I check my basket total
+	Then it should match the price of the laptop
---
+++ features/step_definitions/shop_online_steps.rb
@@ -0,0 +1,39 @@
+Given(/^Amazon\.co\.uk is open$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click Sign\-in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^enter valid user name password$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I am logged in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^Amazon\.co\.uk is open and I am logged in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I search for laptop$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the first result has the word laptop in it$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^I add laptop ASUS ZenBook UX330UA-FB100T to my basket$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I check my basket total$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^it should match the price of the laptop$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
